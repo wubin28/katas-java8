a9f5dfdd4e802ec55cb44724ede1ecb10a3ea529
Ben Wu <wubin28@gmail.com>
2014-09-22 11:28:50 +0800

    Implemented method Apple.getColor().

--- katas-java8/src/main/java/kata/java8/apple/Apple.java
+++ katas-java8/src/main/java/kata/java8/apple/Apple.java
@@ -4,11 +4,14 @@ package kata.java8.apple;
  * Created by ben on 9/22/14.
  */
 public class Apple {
+    private Color color;
+
     public Apple(Color color) {
+        this.color = color;
     }
 
     public Color getColor() {
-        return null;
+        return this.color;
     }
 
     public enum Color {RED, GREEN}
