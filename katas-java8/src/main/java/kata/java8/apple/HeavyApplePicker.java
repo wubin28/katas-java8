5621e6bb44213a5132a826068cccb28773d5c292
Ben Wu <wubin28@gmail.com>
2014-09-22 19:01:20 +0800

    Created method HeavyApplePicker.pick().

--- katas-java8/src/main/java/kata/java8/apple/HeavyApplePicker.java
+++ katas-java8/src/main/java/kata/java8/apple/HeavyApplePicker.java
@@ -1,7 +1,12 @@
 package kata.java8.apple;
 
+import java.util.List;
+
 /**
  * Created by ben on 9/22/14.
  */
 public class HeavyApplePicker {
+    public List<Apple> pick(List<Apple> apples) {
+        return null;
+    }
 }
