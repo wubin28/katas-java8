2913e25ed439fe5c8bc7270755c5375b81fca6b1
Ben Wu <wubin28@gmail.com>
2014-09-22 10:26:53 +0800

    Finished the intent code of theAct and Arrange parts of test the_green_apple_picker_should_pick_green_apples_only().

--- katas-java8/src/test/java/kata/java8/apple/AppleTest.java
+++ katas-java8/src/test/java/kata/java8/apple/AppleTest.java
@@ -2,14 +2,27 @@ package kata.java8.apple;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ben on 9/22/14.
  */
 public class AppleTest {
     @Test
     public void the_green_apple_picker_should_pick_green_apples_only() {
+        // Arrange
+        List<Apple> apples = new ArrayList<Apple>();
+        apples.add(new Apple("Green"));
+        apples.add(new Apple("Red"));
+        apples.add(new Apple("Green"));
+        GreenApplePicker greenApplePicker = new GreenApplePicker();
+
+        // Act
+        List<Apple> pickedApples = greenApplePicker.pick(apples);
+
         // Assert
-        for (Apple apple : apples) {
+        for (Apple apple : pickedApples) {
             assertThat(apple.getColor()).isEqualTo(Apple.GREEN);
         }
     }
