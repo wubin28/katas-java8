9ca91df579a0a04dae96225e59b6e4e0b0694b36
Ben Wu <wubin28@gmail.com>
2014-09-22 22:09:32 +0800

    Made interface Predicate generic.

--- katas-java8/src/main/java/kata/java8/apple/Predicate.java
+++ katas-java8/src/main/java/kata/java8/apple/Predicate.java
@@ -3,6 +3,6 @@ package kata.java8.apple;
 /**
  * Created by ben on 9/22/14.
  */
-public interface Predicate {
-    boolean meetCriteria(Apple apple);
+public interface Predicate<T> {
+    boolean meetCriteria(T t);
 }
