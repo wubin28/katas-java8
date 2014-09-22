cee872c89c1d35d45ef3e31696384cfe2e2c8ccc
Ben Wu <wubin28@gmail.com>
2014-09-22 21:02:49 +0800

    Wrote intent code in method ApplePicker.pick(). Implemented constructor ApplePicker(ApplePickingStrategy).

--- katas-java8/src/main/java/kata/java8/apple/ApplePicker.java
+++ katas-java8/src/main/java/kata/java8/apple/ApplePicker.java
@@ -1,16 +1,25 @@
 package kata.java8.apple;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by ben on 9/22/14.
  */
 public class ApplePicker {
-    public ApplePicker(ApplePickingStrategy applePickingStrategy) {
+    private ApplePickingStrategy applePickingStrategy;
 
+    public ApplePicker(ApplePickingStrategy applePickingStrategy) {
+        this.applePickingStrategy = applePickingStrategy;
     }
 
     public List<Apple> pick(List<Apple> apples) {
-        return null;
+        List<Apple> pickedApples = new ArrayList<Apple>();
+        for (Apple apple : apples) {
+            if (this.applePickingStrategy.meetCriteria()) {
+                pickedApples.add(apple);
+            }
+        }
+        return pickedApples;
     }
 }
