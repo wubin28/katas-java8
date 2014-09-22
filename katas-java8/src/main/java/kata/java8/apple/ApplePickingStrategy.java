f0341ea3a48869af870687f4cf9d957b71146569
Ben Wu <wubin28@gmail.com>
2014-09-22 21:07:17 +0800

    Added method meetCriteria() in interface ApplePickingStrategy. Overrode method GreenApplePickingStrategy.meetCriteria().

--- katas-java8/src/main/java/kata/java8/apple/ApplePickingStrategy.java
+++ katas-java8/src/main/java/kata/java8/apple/ApplePickingStrategy.java
@@ -4,4 +4,5 @@ package kata.java8.apple;
  * Created by ben on 9/22/14.
  */
 public interface ApplePickingStrategy {
+    boolean meetCriteria();
 }
--- katas-java8/src/main/java/kata/java8/apple/GreenApplePickingStrategy.java
+++ katas-java8/src/main/java/kata/java8/apple/GreenApplePickingStrategy.java
@@ -4,4 +4,8 @@ package kata.java8.apple;
  * Created by ben on 9/22/14.
  */
 public class GreenApplePickingStrategy implements ApplePickingStrategy {
+    @Override
+    public boolean meetCriteria() {
+        return false;
+    }
 }
