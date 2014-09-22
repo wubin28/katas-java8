6a4100d9ac4c2592798ed4965639e599e30e068f
Ben Wu <wubin28@gmail.com>
2014-09-22 20:51:11 +0800

    Created method ApplePicker.pick().

--- katas-java8/src/main/java/kata/java8/apple/ApplePicker.java
+++ katas-java8/src/main/java/kata/java8/apple/ApplePicker.java
@@ -1,10 +1,16 @@
 package kata.java8.apple;
 
+import java.util.List;
+
 /**
  * Created by ben on 9/22/14.
  */
 public class ApplePicker {
     public ApplePicker(ApplePickingStrategy applePickingStrategy) {
 
+    }
+
+    public List<Apple> pick(List<Apple> apples) {
+        return null;
     }
 }
