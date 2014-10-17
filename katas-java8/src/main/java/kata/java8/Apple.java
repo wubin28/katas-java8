d96f1dbb738ae49e0cc8e032eadc70347d1a1ecb
Ben Wu <wubin28@gmail.com>
2014-10-17 19:05:00 +0800

    Fixed Error: Created constant Apple.MINIMUM_GRAMS_OF_HEAVY_APPLE.

--- katas-java8/src/main/java/kata/java8/Apple.java
+++ katas-java8/src/main/java/kata/java8/Apple.java
@@ -4,6 +4,7 @@ package kata.java8;
  * Created by ben on 10/17/14.
  */
 public class Apple {
+    public static final int MINIMUM_GRAMS_OF_HEAVY_APPLE = 150;
     private Color color;
 
     public Apple(Color color) {
