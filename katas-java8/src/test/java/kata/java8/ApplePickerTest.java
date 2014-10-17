0d84a5fb12ad1c2d98b5fb6efcf0304b378a627d
Ben Wu <wubin28@gmail.com>
2014-10-18 00:07:06 +0800

    Wrote intention code in the two tests to move method pick() from class ApplePicker to class Apple.

--- katas-java8/src/test/java/kata/java8/ApplePickerTest.java
+++ katas-java8/src/test/java/kata/java8/ApplePickerTest.java
@@ -18,10 +18,9 @@ public class ApplePickerTest {
         apples.add(new Apple(Apple.Color.GREEN));
         apples.add(new Apple(Apple.Color.RED));
         apples.add(new Apple(Apple.Color.GREEN));
-        ApplePicker applePicker = new ApplePicker();
 
         // Act
-        List<Apple> pickedApples = applePicker.pick(apples, (Apple apple) -> apple.getColor() == Apple.Color.GREEN);
+        List<Apple> pickedApples = Apple.pick(apples, (Apple apple) -> apple.getColor() == Apple.Color.GREEN);
 
         // Assert
         assertThat(pickedApples.size()).isEqualTo(2);
@@ -39,8 +38,7 @@ public class ApplePickerTest {
         apples.add(new Apple(Apple.MINIMUM_GRAMS_OF_HEAVY_APPLE - 10));
 
         // Act
-        ApplePicker applePicker = new ApplePicker();
-        List<Apple> pickedApples = applePicker.pick(apples, (Apple apple) -> apple.getWeight() >= Apple.MINIMUM_GRAMS_OF_HEAVY_APPLE);
+        List<Apple> pickedApples = Apple.pick(apples, (Apple apple) -> apple.getWeight() >= Apple.MINIMUM_GRAMS_OF_HEAVY_APPLE);
 
         // Assert
         assertThat(pickedApples.size()).isEqualTo(1);
