c7a1cc8dec0d6fa4dc8319a0c2ce44064a49d390
Ben Wu <wubin28@gmail.com>
2014-10-17 11:25:57 +0800

    Wrote Intention code to try if Fest with JUnit4 is working.

---
+++ katas-java8/src/test/java/kata/java8/ApplePickerTest.java
@@ -0,0 +1,11 @@
+package kata.java8;
+
+/**
+ * Created by ben on 10/17/14.
+ */
+public class ApplePickerTest {
+    @Test
+    public void the_green_apple_picker_should_pick_green_apples_only() {
+        assertThat(2 + 3).isEqualTo(5);
+    }
+}
