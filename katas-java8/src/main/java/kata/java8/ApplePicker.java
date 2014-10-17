1d2f80efc4ea22f23f4a4e0f9fe6047432af6e9f
Ben Wu <wubin28@gmail.com>
2014-10-17 22:07:03 +0800

    Fixed Error: Created method ApplePicker.pick().

--- katas-java8/src/main/java/kata/java8/ApplePicker.java
+++ katas-java8/src/main/java/kata/java8/ApplePicker.java
@@ -1,7 +1,12 @@
 package kata.java8;
 
+import java.util.List;
+
 /**
  * Created by ben on 10/17/14.
  */
 public class ApplePicker {
+    public List<Apple> pick(List<Apple> apples, ApplePickingStrategy applePickingStrategy) {
+        return null;
+    }
 }
