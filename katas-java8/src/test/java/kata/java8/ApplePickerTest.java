687b4bad7f8a58a919d4b8a61f83cfe7362ec890
Ben Wu <wubin28@gmail.com>
2014-10-17 12:17:32 +0800

    Finished intention code for the Assert, Act and Arrange parts of test the_green_apple_picker_should_pick_green_apples_only().

--- katas-java8/src/test/java/kata/java8/ApplePickerTest.java
+++ katas-java8/src/test/java/kata/java8/ApplePickerTest.java
@@ -10,8 +10,18 @@ import static org.fest.assertions.Assertions.assertThat;
 public class ApplePickerTest {
     @Test
     public void the_green_apple_picker_should_pick_green_apples_only() {
+        // Arrange
+        List<Apple> apples = new ArrayList<Apple>();
+        apples.add(new Apple(Apple.Color.GREEN));
+        apples.add(new Apple(Apple.Color.RED));
+        apples.add(new Apple(Apple.Color.GREEN));
+
+        // Act
+        List<Apple> pickedApples = greenApplePicker(apples);
+
         // Assert
-        for (Apple apple : apples) {
+        assertThat(pickedApples.size()).isEqualTo(2);
+        for (Apple apple : pickedApples) {
             assertThat(apple.getColor()).isEqualTo(Apple.Color.GREEN);
         }
     }
