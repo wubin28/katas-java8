dc7552f8a7adfed755fd7d780d46f9c28c704b01
Ben Wu <wubin28@gmail.com>
2014-10-17 20:41:17 +0800

    Refactored: Made class HeavyApplePicker extend class ApplePicker.

--- katas-java8/src/main/java/kata/java8/HeavyApplePicker.java
+++ katas-java8/src/main/java/kata/java8/HeavyApplePicker.java
@@ -6,18 +6,10 @@ import java.util.List;
 /**
  * Created by ben on 10/17/14.
  */
-public class HeavyApplePicker {
-    public List<Apple> pick(List<Apple> apples) {
-        List<Apple> pickedApples = new ArrayList<>();
-        for (Apple apple : apples) {
-            if (meetCriteria(apple)) {
-                pickedApples.add(apple);
-            }
-        }
-        return pickedApples;
-    }
+public class HeavyApplePicker extends ApplePicker {
 
-    private boolean meetCriteria(Apple apple) {
+    @Override
+    protected boolean meetCriteria(Apple apple) {
         return apple.getWeight() >= Apple.MINIMUM_GRAMS_OF_HEAVY_APPLE;
     }
 }
