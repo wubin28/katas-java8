e3bb16fa4690f54d13a6e8db5004b9f39e97d827
Ben Wu <wubin28@gmail.com>
2014-10-17 23:12:38 +0800

    Wrote intention code to use method reference in the two tests.

--- katas-java8/src/test/java/kata/java8/ApplePickerTest.java
+++ katas-java8/src/test/java/kata/java8/ApplePickerTest.java
@@ -21,12 +21,7 @@ public class ApplePickerTest {
         ApplePicker applePicker = new ApplePicker();
 
         // Act
-        List<Apple> pickedApples = applePicker.pick(apples, new ApplePickingStrategy() {
-            @Override
-            protected boolean meetCriteria(Apple apple) {
-                return apple.getColor() == Apple.Color.GREEN;
-            }
-        });
+        List<Apple> pickedApples = applePicker.pick(apples, Apple::isGreenApple);
 
         // Assert
         assertThat(pickedApples.size()).isEqualTo(2);
@@ -45,12 +40,7 @@ public class ApplePickerTest {
 
         // Act
         ApplePicker applePicker = new ApplePicker();
-        List<Apple> pickedApples = applePicker.pick(apples, new ApplePickingStrategy() {
-            @Override
-            protected boolean meetCriteria(Apple apple) {
-                return apple.getWeight() >= Apple.MINIMUM_GRAMS_OF_HEAVY_APPLE;
-            }
-        });
+        List<Apple> pickedApples = applePicker.pick(apples, Apple::isHeavyApple);
 
         // Assert
         assertThat(pickedApples.size()).isEqualTo(1);
