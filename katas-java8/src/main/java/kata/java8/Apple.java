6c908032e3c78e19d2483d6de8dc2d8c881149f8
Ben Wu <wubin28@gmail.com>
2014-10-18 13:43:21 +0800

    Fixed Error: Created constructor Apple(String, int).

--- katas-java8/src/main/java/kata/java8/Apple.java
+++ katas-java8/src/main/java/kata/java8/Apple.java
@@ -20,6 +20,10 @@ public class Apple {
         this.weight = weight;
     }
 
+    public Apple(String varietyName, int weightInGrams) {
+
+    }
+
     public Color getColor() {
         return this.color;
     }
